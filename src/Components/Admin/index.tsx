062d94d5cd7fb62da4026f0bf908b17be8bbd9a5
keil <keil@keilovergames.com>
2019-08-01 11:18:20 -0700

    readme upate and psuedo code added for unfinshed admin page

--- src/Components/Admin/index.tsx
+++ src/Components/Admin/index.tsx
@@ -5,7 +5,7 @@ interface Props {}
 
 interface State {
 }
-
+/*semi psuedo coded this class would be very similar to helpdesk*/
 export class Admin extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
@@ -16,10 +16,17 @@ export class Admin extends React.Component<Props, State> {
     sessionStorage.clear();
     console.log(sessionStorage);
   } 
-
+   /*create on change funciton similar to helpdesk but ignore where clause
+    instead grab the data check if user permission is admin and use put a method 
+    to update selected and edited ticket
+        */
   render() {
     return(
       <>
+        {/*import dynamic create card for every ticket within ticket 
+        database using a get method with my sql queury of 
+        SELECT * FROM ticket
+        */}
         <h1>Admin DashBoard</h1>
         <Link to="/" onClick={this.clearStorage}>logOut</Link>
 
