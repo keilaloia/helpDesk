83f83daea8118f25a3f8ac4be692ed6e0edd9c27
keil <keil@keilovergames.com>
2019-08-07 13:33:46 -0700

    Updates

---
+++ precache-manifest.e03b6ce299793eca941d0975ee50bb98.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3fa2255f6b62130982d363d0f605ee9",
+    "url": "/helpDesk/index.html"
+  },
+  {
+    "revision": "81743fa369843dc1ca4e",
+    "url": "/helpDesk/static/css/main.43ab4f4f.chunk.css"
+  },
+  {
+    "revision": "721a723dd2f4bac52182",
+    "url": "/helpDesk/static/js/2.20665b99.chunk.js"
+  },
+  {
+    "revision": "81743fa369843dc1ca4e",
+    "url": "/helpDesk/static/js/main.d8dc9eae.chunk.js"
+  },
+  {
+    "revision": "5d7757326536dc8fcf40",
+    "url": "/helpDesk/static/js/runtime~main.68d417dc.js"
+  }
+]);
